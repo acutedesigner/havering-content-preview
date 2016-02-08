d20452a42ef3f3741a705178fd2c9eecc5e1b047
Nigel M Peters <develop@acumendesign.co.uk>
2016-02-08 18:14:16 +0000

    Updated the Custom Post Types generator class

--- wp-content/plugins/intranet-plugins/intranet-plugins.php
+++ wp-content/plugins/intranet-plugins/intranet-plugins.php
@@ -12,8 +12,8 @@ License: GPL2
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 define("CPT_VERSION_NUMBER", "0.1");
-define("CPT_PLUGIN_DIR", plugin_dir_path(__FILE__)); 
-define("CPT_PLUGIN_URL", plugin_dir_url( __FILE__ )); 
+define("CPT_PLUGIN_DIR", plugin_dir_path(__FILE__));
+define("CPT_PLUGIN_URL", plugin_dir_url( __FILE__ ));
 
 //Load up the classes for wordpress
-require_once CPT_PLUGIN_DIR . 'includes/custom-posttypes.php';
+require_once CPT_PLUGIN_DIR . 'includes/class-custom-posttypes.php';
